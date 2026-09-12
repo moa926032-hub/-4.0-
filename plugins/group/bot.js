469f88e32ba750eb33af2aaf66d033fbc503205b
𝑴𝑶 𝑭𝑶𝑹𝑨1𝑶𝑵 <moa926032@gmail.com>
2026-09-12 17:18:46 +0300

    Delete plugins directory

--- plugins/group/bot.js
+++
@@ -1,8 +0,0 @@
-let handler = async (m, { conn }) => {
-    await conn.circular(m.chat, { vid: "https://files.catbox.moe/bqxzcl.mp4", sec: 200 }, m);
-}
-
-handler.command = ["بوم"];
-handler.usePrefix = false;
-
-export default handler;
